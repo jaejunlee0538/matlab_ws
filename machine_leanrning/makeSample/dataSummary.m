29d1cee165daf288736370de13ccd5b97b3d36e8
jaejunlee <jaejun0201@gmail.com>
2015-05-05 14:52:45 +0900

    clearing

--- machine_leanrning/makeSample/dataSummary.m
+++
@@ -1,7 +0,0 @@
-function  dataSummary( data )
-for i=1:1:4
-    fprintf('class %d = %d\n',i,sum(data(3,:)==i));
-end
-fprintf('total : %d\n',length(data));
-end
-
--- machine_leanrning/makeSample/sample_gaussian.m
+++
@@ -1,7 +0,0 @@
-function M = sample_gaussian(mu, Sigma, N)
-mu = mu(:);
-n=length(mu);
-[U,D,V] = svd(Sigma);
-M = randn(n,N);
-M = (U*sqrt(D))*M + mu*ones(1,N); 
-M = M';
